5ff527cebe27c0c46822f20e00225d59bdecf66d
Sumankeshav <sumankeshav.22887@gmail.com>
2025-12-31 20:18:41 +0530

    assignment1

--- Problems/task1.py
+++ Problems/task1.py
@@ -1,3 +1,15 @@
-//add
+num1=float(input("enter first number"))
+num2=float(input("enter second number"))
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
 
-// credits
+if num2 !=0:
+    div=num1/num2
+else:
+    div="Cannot divide by zero"
+
+print("The sum is",add)
+print("The difference is",sub)
+print("The multiplication is",mul)
+print("The division is",div)
---
+++ Problems/task2.py
@@ -0,0 +1,5 @@
+name1 = input("Enter your first name: ")
+name2 = input("Enter your last name: ")
+
+con = name1 + " " + name2
+print("Hello,", con, "Welcome to python program")
